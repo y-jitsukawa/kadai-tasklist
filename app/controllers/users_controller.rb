bd0c69a214edafbd45f4df629b3c13b1801599c9
y_jitsukawa <y_jitsukawa_chi@yahoo.co.jp>
2020-09-12 09:52:37 +0000

    post2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-     @tasks = @user.tasks.order(id: :desc).page(params[:page])
+    @tasks = @user.tasks.order(id: :desc).page(params[:page])
     counts(@user)
   end
 
