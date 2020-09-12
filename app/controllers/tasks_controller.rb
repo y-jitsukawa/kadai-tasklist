ea0f4d225bd6873fec23a359608dc74c1918e212
y_jitsukawa <y_jitsukawa_chi@yahoo.co.jp>
2020-09-12 12:22:23 +0000

    post3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,11 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy]
+    before_action :correct_user, only: [:new, :edit, :destroy]
     
     def index
-        if logged_in?
             @task = current_user.tasks.build
             @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-        end
     end
     
     def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in
-  
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc).page(params[:page])
-    counts(@user)
-  end
 
   def new
     @user = User.new
